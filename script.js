f5df582831a0ac5b80fb01b8b16e51df5e7267f4
Dinal02 <99264322+Dinal02@users.noreply.github.com>
2022-03-31 21:22:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//1. Create a variable for each data type
+//2. Use all the variable in a consol.log and get the output
+
+//String
+var a = "Skill Safari"
+console.log(a)
+
+//INT
+var b = 40
+console.log(b)
+
+//FLOAT
+var c = 4.8
+console.log(c)
+
+//Boolean
+var d = 34
+var e = 59
+var f = d>e
+console.log(f)
+
+//3. Using VAR, LET and CONST, find the solution for the following values. 
+//i) Area of a circle
+var r = 5 // 5cm
+let pi = 3.14
+const area = pi*r*r
+
+console.log(area + " sq. cm")
+
+//ii) Circumference of a circle
+const circum = 2*pi*r
+
+console.log(circum + " cm")
+
+//iii) Radius of the circle
+console.log(r + "cm")
